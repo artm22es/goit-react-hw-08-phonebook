ca9818482fc4cbd9b4e961755a06a3fe37867bc7
artm22es <artm22es@gmail.com>
2024-01-25 17:22:53 +0100

    Update HomeSection.jsx

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -1,5 +1,4 @@
 import { useAuth } from 'hooks/useAuth';
-import { Link } from 'react-router-dom';
 import { LoginForm } from 'components/LoginForm/LoginForm';
 import Contacts from 'pages/Contacts/Contacts';
 
